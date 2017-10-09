3b0bc69b1c26e2492899c81b31a049e96ea0c0d2
Tarmicle <enrico.bottani@hotmail.com>
2017-10-09 08:21:47 +0200

    Program arguments file existence check

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 void pour_file_to(char *args, void (*output_handler)(unsigned char)){
@@ -19,22 +20,29 @@ void print_char(unsigned char c){
 }
 
 int main(int args_number, char *args[]) {
-    //DEBUG
-    for (int i = 1; i < args_number; ++i) {
-        printf("arg[%d]: %s\n", i, args[i]);
+
+    if(args_number!=3){
+        printf("usage: a.out option source_file");
+        exit(2);
     }
 
     if(strcmp("-c",args[1])==0){
-        //COMPRESSION
+        // COMPRESSION
         // ac_encode(lz77_encode(args[1]));
     }
     else if(strcmp("-d",args[1])==0){
-        //DECOMPRESSION
+        // DECOMPRESSION
         // lz77_decode(ac_decode(args[1]));
     }
     else {
-        printf("Invalid option");
-        exit(0);
+        printf("Error: Invalid option '%s'", args[1]);
+        exit(2);
+    }
+    // Check if file exists
+    if( access(args[2], F_OK ) == -1 ) {
+        // file doesn't exist
+        printf("Error: File not found");
+        exit(2);
     }
 
     pour_file_to(args[2], print_char);
