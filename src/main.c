31c5b5eaaa001c41e3bbbcaba3a7b3281ab1402b
Tarmicle <enrico.bottani@hotmail.com>
2017-10-09 07:15:36 +0200

    "Pour file to" function

--- src/main.c
+++ src/main.c
@@ -3,35 +3,41 @@
 #include <string.h>
 
 
-void open_file(char args[]){
+void pour_file_to(char *args, void (*output_handler)(unsigned char)){
     FILE *input;
     input = fopen(args, "r");
 
     int next_char = fgetc(input);
     while (next_char != EOF) {
-        printf("%c",next_char);
+        output_handler((unsigned char)next_char);
         next_char = fgetc(input);
     }
 }
 
+void print_char(unsigned char c){
+    printf("%c",c);
+}
+
 int main(int args_number, char *args[]) {
-    for (int i = 0; i < args_number; ++i)
-    {
-        printf("argv[%d]: %s\n", i, args[i]);
+    //DEBUG
+    for (int i = 1; i < args_number; ++i) {
+        printf("arg[%d]: %s\n", i, args[i]);
     }
 
     if(strcmp("-c",args[1])==0){
         //COMPRESSION
+        // ac_encode(lz77_encode(args[1]));
     }
     else if(strcmp("-d",args[1])==0){
         //DECOMPRESSION
+        // lz77_decode(ac_decode(args[1]));
     }
     else {
         printf("Invalid option");
         exit(0);
     }
 
-    open_file(args[2]);
+    pour_file_to(args[2], print_char);
 
     return 0;
 }
