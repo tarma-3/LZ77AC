e82d7fa8ef55debda2d8488a3d564b97402ce37f
Tarmicle <enrico.bottani@hotmail.com>
2017-10-09 18:17:27 +0200

    Rename pour_file_to to stream_file_to & error handling improvements

--- src/main.c
+++ src/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <errno.h>
 
-
-void pour_file_to(char *args, void (*output_handler)(unsigned char)){
+int stream_file_to(char *args,void (*output_handler)(unsigned char)){
+    // This function return 1 if the file exists
     FILE *input;
-    input = fopen(args, "r");
-
-    int next_char = fgetc(input);
-    while (next_char != EOF) {
-        output_handler((unsigned char)next_char);
-        next_char = fgetc(input);
+    int next_char;
+    if((input = fopen(args, "r"))!=0&&((next_char = fgetc(input))>=0)) {
+        while (next_char != EOF) {
+            output_handler((unsigned char) next_char);
+            next_char = fgetc(input);
+        }
+        fclose(input);
+        return 1;
     }
+    return 0;
 }
 
 void print_char(unsigned char c){
@@ -26,6 +29,7 @@ int main(int args_number, char *args[]) {
         exit(2);
     }
 
+    // Option check
     if(strcmp("-c",args[1])==0){
         // COMPRESSION
         // ac_encode(lz77_encode(args[1]));
@@ -35,17 +39,15 @@ int main(int args_number, char *args[]) {
         // lz77_decode(ac_decode(args[1]));
     }
     else {
-        printf("Error: Invalid option '%s'", args[1]);
-        exit(2);
+        fprintf(stderr,"Error on first parameter '%s': Invalid option", args[1]);
+        exit(107);
     }
     // Check if file exists
-    if( access(args[2], F_OK ) == -1 ) {
-        // file doesn't exist
-        printf("Error: File not found");
-        exit(2);
+    if(!stream_file_to(args[2],print_char)){
+        // If file not exists
+        fprintf(stderr,"Error on 2nd parameter '%s': %s\n",args[2],strerror(errno));
+        exit(errno);
     }
-
-    pour_file_to(args[2], print_char);
 
     return 0;
 }
