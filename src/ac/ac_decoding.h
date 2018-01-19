909c126e086c33a1c73c817f48772a0a738b0431
alice_nesurini <alice.nesurini@gmail.com>
2018-01-19 13:16:56 +0100

    Changed .h function fix

--- src/ac/ac_decoding.h
+++ src/ac/ac_decoding.h
@@ -15,10 +15,6 @@ void set_frequency(int *frq, int len);
 
 void set_total_char(int t_ch);
 
-void read_in_32();
-
-void set_to_counter_EOF();
-
 void init_wa(char *ac_output);
 
 #endif //LZ77AC_AC_DECODING_H
